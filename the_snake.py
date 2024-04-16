b8ed1c8e7da68b7bd4a8ea817c42737499bfcfe4
Nikita Prokhvatilov <IvensGin@yandex.ru>
2024-04-16 18:10:44 +0300

    Snake move complete

--- the_snake.py
+++ the_snake.py
@@ -90,26 +90,19 @@ class Snake(GameObject):
             head_pos[0] + self.direction[0] * GRID_SIZE,
             head_pos[1] + self.direction[1] * GRID_SIZE)
         # столкновение c правым краем
-        if head_pos[0] == SCREEN_WIDTH:
-            new_head = (GRID_SIZE, head_pos[1])
-            self.positions.insert(0, new_head)
-            
+        if head_pos[0] > SCREEN_WIDTH:
+            new_head = (0, head_pos[1])
         # столкновение c левым краем
-        if head_pos[0] == 0:
+        elif head_pos[0] < 0:
             new_head = (SCREEN_WIDTH, head_pos[1])
-            self.positions.insert(0, new_head)
-
             # столкновение c верхним краем
-        if head_pos[1] == 0:
+        if head_pos[1] < 0:
             new_head = (head_pos[0], SCREEN_HEIGHT)
-            self.positions.insert(0, new_head)
-            
         # столкновение c нижним краем
-        if head_pos[1] == SCREEN_HEIGHT:
+        elif head_pos[1] > SCREEN_HEIGHT:
             new_head = (head_pos[0], GRID_SIZE)
-            self.positions.insert(0, new_head)
+        # удаление последнего сегмента
         self.positions.insert(0, new_head)
-
         if len(self.positions) > self.length:
             self.last = self.positions.pop()           
 
